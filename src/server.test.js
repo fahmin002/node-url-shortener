ebd4852b232c3eae0e3fdc290cb0a0321b7ab713
fahminabilmaulana <fahminabilmaulana@gmail.com>
2025-02-06 21:25:08 +0700

    "adding comment to server.test"

--- src/server.test.js
+++ src/server.test.js
@@ -1,25 +1,32 @@
 import request from 'supertest';
 import app from './server';
+const fullUrlExample = process.env.fullUrl
+const shortUrlExample = process.env.shortUrl
 
 test(`GET '/' should return 200`, async () => {
-    const response = await request(app).get('/');
-    expect(response.status).toBe(200);
+  // request home page, and return 200 as status code, it's mean home page successfully rendered
+  const response = await request(app).get('/');
+  expect(response.status).toBe(200);
 });
 
 test('POST \'/:shortUrl\' should return 200', async () => {
-  const payload = { fullUrl: 'https://stackoverflow.com/questions/55424501/passing-request-body-to-post-using-super-test' }
+  // prepared payload for body, to send into server
+  const payload = { fullUrl: fullUrlExample }
+  // post request, convert object into url string for send to server as x-www-form-urlencoded
   const response = 
   await request(app)
   .post('/shortUrl')
   .send(new URLSearchParams(payload).toString())
   .set('Content-Type', 'application/x-www-form-urlencoded')
   .set('Accept', 'application/x-www-form-urlencoded');
+  // expect the server will redirect the page to home page
   expect(response.status).toBe(302);
 });
 
+// expect server will redirect the page to real url
 test('GET \'/:shortenedUrl should return 302', async () => {
     const response =
     await request(app)
-    .get('/dvkmy_9j6');
+    .get(`/${shortUrlExample}`);
     expect(response.status).toBe(302);
 })
